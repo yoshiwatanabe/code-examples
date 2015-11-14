91dbe88cf6fe234a85ff226cebbaf17539190c76
Yoshi Watanabe <twatana@microsoft.com>
2015-11-14 10:24:24 -0800

    added an example for Gulp watch

---
+++ gulp/basic-5/gulpfile.js
@@ -0,0 +1,18 @@
+// npm install gulp gulp-concat
+
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+
+gulp.task('compile', function() {
+    return gulp.src('scripts/**/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist'));
+});
+
+// Whenever any change is detected in a js file, we run the compile task.
+gulp.task('watch', function() {
+  gulp.watch('scripts/**/*.js', ['compile']);
+});
+
+// Default Task
+gulp.task('default', ['compile', 'watch']);
