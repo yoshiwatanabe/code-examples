0c20e17ae6f002ad0e548c1737130c76e3589d30
Yoshi Watanabe <yoshi@github-noreply.com>
2017-06-13 21:04:21 -0700

    docker example

---
+++ docker/simple-console/ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.WriteLine((args.Any() ? 
+                String.Join(Environment.NewLine, args) : "<no argument>"));
+        }
+    }
+}
