29d5dbc633b9f245cfa6074959152c92e2d6dd73
Yoshi Watanabe <yoshi@yoshi.com>
2020-05-30 12:27:18 -0700

    add cds examples

---
+++ react/experiments/my-app-basic-ejected/src/env.variables.js
@@ -0,0 +1,8 @@
+const ChunkNames = {
+    MyInsightsPage: 'yoshi_insights_report',
+    MySettingsPage: 'yoshi_insights_settings'
+};
+
+module.exports = {
+    ChunkNames
+};
--- react/experiments/my-app-basic/src/App.tsx
+++ react/experiments/my-app-basic/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ChunkNames } from './environment-variables';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          x Edit <code>src/App.tsx</code> and save to reload. {ChunkNames.MySettingsPage}
         </p>
         <a
           className="App-link"
